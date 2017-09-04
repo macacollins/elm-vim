f9c0e4d80ab5a0dbd9ecfbc2181dd6c14a2fe0a2
Mac Collins <macacollins@gmail.com>
2017-09-04 11:19:58 -0500

    Add basic cursor tests. Will need to add more as more functionality gets added.

--- src/Handlers/NewLine.elm
+++ src/Handlers/NewLine.elm
@@ -3,19 +3,22 @@ module Handlers.NewLine exposing (handleNewLine)
 import Model exposing (Model)
 import Array exposing (..)
 
+
 handleNewLine : Model -> Model
 handleNewLine model =
-  let
-      sliceIndex = (model.cursorY + 1)
+    let
+        sliceIndex =
+            (model.cursorY + 1)
 
-      updatedLines = 
-          Array.append (slice 0 sliceIndex model.lines) <|
-          Array.append (fromList [ "" ])
-          (slice sliceIndex (Array.length model.lines) model.lines)
+        updatedLines =
+            Array.append (slice 0 sliceIndex model.lines) <|
+                Array.append (fromList [ "" ])
+                    (slice sliceIndex (Array.length model.lines) model.lines)
 
-      newCursorY =
-          model.cursorY + 1
+        newCursorY =
+            model.cursorY + 1
 
-  in
-      { model | lines = updatedLines, cursorY = newCursorY }
-
+        newCursorX =
+            0
+    in
+        { model | lines = updatedLines, cursorY = newCursorY, cursorX = newCursorX }
--- src/View.elm
+++ src/View.elm
@@ -4,60 +4,64 @@ import Html exposing (..)
 import Model exposing (Model)
 import Array
 import Styles exposing (styleString)
-
 import Html.Attributes exposing (class, id)
 
+
 view : Model -> Html msg
-view model = 
-  let
-    lines =
-      Array.toList <| Array.indexedMap (getLine model) model.lines
+view model =
+    let
+        lines =
+            Array.toList <| Array.indexedMap (getLine model) model.lines
 
-    styles =
-      node "style" [] [ text styleString ]
+        styles =
+            node "style" [] [ text styleString ]
 
-    children = styles :: lines
+        children =
+            styles :: lines
+    in
+        main_ [] children
 
-  in 
-    main_ [] children
 
 getLine : Model -> Int -> String -> Html msg
 getLine model index line =
-  let
-    className = 
-      if model.cursorY == index then
-        "selectedLine"
-      else
-        "normalLine"
+    let
+        className =
+            if model.cursorY == index then
+                "selectedLine"
+            else
+                "normalLine"
 
-    textContents =
-      if model.cursorY == index then
-        if String.length line == 0 then
-          span [ id "cursor" ] []
-        else
-          let 
-              before = String.slice 0 model.cursorX line
-              maybeMiddle = String.slice model.cursorX (model.cursorX + 1) line 
-              after = String.slice (model.cursorX + 1) (String.length line + 1) line 
-
-              middle =
-                if String.length maybeMiddle == 0 then
-                  " "
+        textContents =
+            if model.cursorY == index then
+                if String.length line == 0 then
+                    span [ id "cursor" ] [ text "_" ]
                 else
-                  maybeMiddle
+                    let
+                        before =
+                            String.slice 0 model.cursorX line
 
-          in
-            span []
-              [ text <| before
-              , span [ id "cursor" ] [ text <| middle ]
-              , text <| after
-              ]
+                        maybeMiddle =
+                            String.slice model.cursorX (model.cursorX + 1) line
 
-      else
-          text line
-  in
-    div [ class className ] 
-      [ span [] [ text <| toString index ]
-      , textContents
-      ]
+                        after =
+                            String.slice (model.cursorX + 1) (String.length line + 1) line
 
+                        middle =
+                            Debug.log "Cursor Text" <|
+                                if String.length maybeMiddle == 0 then
+                                    "_"
+                                else
+                                    maybeMiddle
+                    in
+                        span []
+                            [ text <| before
+                            , span [ id "cursor" ] [ text <| middle ]
+                            , text <| after
+                            ]
+            else
+                text line
+    in
+        div [ class className ]
+            [ span [] [ text <| toString index ]
+            , textContents
+            ]
